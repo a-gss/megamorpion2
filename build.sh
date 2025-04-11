9d3e6cc91240773d89e682026e7f0b89af1ce91a
a-gss <36766244+a-gss@users.noreply.github.com>
2025-04-11 14:14:54 +0200

    Added stripping to build.sh + misc fixes

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-OS=$(uname) # Detect OS
+OS=$(uname)
 
 # Default libraries and flags
-LIBS="-L$(pwd)/ncurses/lib -lncursesw -ltinfow -lm"
-LIBS_DEBUG="-L$(pwd)/ncurses/lib -lncursesw_g -ltinfow_g -lm"
+LIBS="$(pwd)/ncurses/lib -lncursesw -ltinfow -lm"
+LIBS_DEBUG="$(pwd)/ncurses/lib -lncursesw_g -ltinfow_g -lm"
 
 # Compilation flags
 WARNINGS="-Wall -Wextra -Wpedantic"
@@ -12,12 +12,12 @@ CFLAGS="-DNCURSES_WIDECHAR"
 INCLUDE_DIR="-I$(pwd)/ncurses/include/ncursesw -I$(pwd)/ncurses/include"
 
 # Platform-specific
-if   [ "$OS" = "Darwin" ]; then
+if [ "$OS" = "Darwin" ]; then
 	LINKER_FLAGS="-Wl,-search_paths_first"
 	CFLAGS+=' -D_DARWIN_C_SOURCE'
 	cc='clang'
 elif [ "$OS" = "Linux" ];  then
-	LINKER_FLAGS="--search_paths_first"
+	LINKER_FLAGS=""
 	cc='gcc'
 else
 	echo "Unsupported OS: $OS" ;
@@ -28,7 +28,8 @@ fi
 for arg in "$@"; do
     case $arg in
         -r|--release)
-            WARNINGS+="-Werror"
+            WARNINGS+=" -Werror"
+            STRIP_BINARY=true
             ;;
         -d|--debug)
             DEBUG="-g"
@@ -41,4 +42,12 @@ for arg in "$@"; do
     esac
 done
 
-$cc $WARNINGS $CFLAGS $INCLUDE_DIR $DEBUG all.c $LIBS -o all
+in='all.c'
+out='all'
+
+# Final command
+$cc $WARNINGS $CFLAGS $INCLUDE_DIR $DEBUG $in $LINKER_FLAGS -L$LIBS -o $out
+
+if [ "$STRIP_BINARY" = true ]; then
+    strip $out
+fi
