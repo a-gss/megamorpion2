21c8c507942267eb8139e70ecc9fd2208379f264
a-gss <36766244+a-gss@users.noreply.github.com>
2025-04-11 14:03:30 +0200

    WSL compat

--- all.c
+++ all.c
@@ -1,7 +1,9 @@
+#include <stdlib.h>
 #include "ncurses.h"
 
 int main(void)
 {
+	setenv("TERM", "vt100", 1);  // Remplace "xterm" par "vt100" si nécessaire
     initscr();
     addstr("Goodbye, cruel C programming!");
     refresh();
--- build.sh
+++ build.sh
@@ -1,19 +1,44 @@
 #!/bin/sh
-WARNINGS="-Wall -Wextra -Wpedantic"
-CFLAGS="-D_DARWIN_C_SOURCE -DNCURSES_WIDECHAR"
-INCLUDE_DIR="-I$(pwd)/ncurses/include/ncursesw -I$(pwd)/ncurses/include"
-LIBS="-L$(pwd)/ncurses/lib -Wl,-search_paths_first -lncursesw -ltinfow -lm"
+set -e
+OS=$(uname) # Detect OS
+
+# Default libraries and flags
+LIBS="-L$(pwd)/ncurses/lib -lncursesw -ltinfow -lm"
 LIBS_DEBUG="-L$(pwd)/ncurses/lib -lncursesw_g -ltinfow_g -lm"
 
-while getopts ":hcdvsr" opt; do
-    case $opt in
-        r | release) WARNINGS+="-Werror" ;;
-        d | debug)
+# Compilation flags
+WARNINGS="-Wall -Wextra -Wpedantic"
+CFLAGS="-DNCURSES_WIDECHAR"
+INCLUDE_DIR="-I$(pwd)/ncurses/include/ncursesw -I$(pwd)/ncurses/include"
+
+# Platform-specific
+if   [ "$OS" = "Darwin" ]; then
+	LINKER_FLAGS="-Wl,-search_paths_first"
+	CFLAGS+=' -D_DARWIN_C_SOURCE'
+	cc='clang'
+elif [ "$OS" = "Linux" ];  then
+	LINKER_FLAGS="--search_paths_first"
+	cc='gcc'
+else
+	echo "Unsupported OS: $OS" ;
+	exit 1
+fi
+
+# Handle arguments
+for arg in "$@"; do
+    case $arg in
+        -r|--release)
+            WARNINGS+="-Werror"
+            ;;
+        -d|--debug)
             DEBUG="-g"
             LIBS=$LIBS_DEBUG
             ;;
-        \?) echo "Invalid option: -$OPTARG" >&2 ; exit 1 ;;
+        *) 
+            echo "Unknown option: $arg"
+            exit 1
+            ;;
     esac
 done
 
-clang $WARNINGS $CFLAGS $INCLUDE_DIR $DEBUG all.c $LIBS -o all
+$cc $WARNINGS $CFLAGS $INCLUDE_DIR $DEBUG all.c $LIBS -o all
