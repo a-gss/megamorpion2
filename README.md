9b4d0c65e6e6edc85549eb36e259f71b68b21ed4
agss <36766244+discomeduse@users.noreply.github.com>
2025-06-22 19:52:25 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # megamorpion2
-Second attempt at making a TUI [super tic-tac-toe game](https://youtu.be/_Na3a1ZrX7c)
+Second attempt at making a TUI [super tic-tac-toe game](https://youtu.be/_Na3a1ZrX7c).
+
+First attempt: [a-gss/megamorpion](https://github.com/a-gss/megamorpion)
 
 ## Building
-The `build-ncurses.sh` script downloads `ncurses-6.5` and build it from source statically in `ncurses/`. Just run the script:
+The `build-ncurses.sh` script downloads `ncurses-6.5` and builds it from source statically in `ncurses/`. Just run the script:
 ```bash
 ./build-ncurses.sh
 ```
-Then you can compile megamporpion2 (which btw is a single translation unit) and link it against ncurses using the build script:
+Then you can compile megamporpion2 and link it against ncurses using the build script:
 ```bash
 ./build.sh
 ```
 
 ## Extended rules
-There are no offical rules, so I just made some up because I find them fun:
+There are no official rules, so I just made some up because I find them fun (and I had to):
 
-1. When being teleported to the next zone, the coordinates of the selector are random
+1. When being teleported to the next zone, the local coordinates of the cursor are random
 2. If the next zone is blocked, ie. the sub-game is finished, then the player can choose whatever zone to go to
 3. If there is a tie on the mega-game, the winner is the one who has won the most sub-games
--- megamorpion.h
+++ megamorpion.h
@@ -1,3 +1,7 @@
+#include "ncurses/include/ncursesw/ncurses.h"
+#define PLAYER1 1
+#define PLAYER2 2
+
 /* MAC-OS AZERTY */
 #define KEY_ENTER           10
 #define KEY_ESCAPE          27
@@ -13,7 +17,20 @@
 #define KEY_E_ACCENT_AIGU  169
 #define TERM_RESIZE        410
 
-void print_header(int ch) {
+typedef struct {
+    unsigned int x;
+    unsigned int y;
+} vec2;
+
+typedef struct {
+    unsigned int bigX;
+    unsigned int bigY;
+    unsigned int smallX;
+    unsigned int smallY;
+} cursorPos_t;
+
+void print_header(int ch)
+{
     attron(A_DIM);
         mvprintw(0,0, "Press [esc] to quit.");
         mvprintw(0, COLS-3, "[?]");                   // je voulais rajouter un menu qui explique les regles/les commandes
@@ -23,7 +40,8 @@ void print_header(int ch) {
     attroff(A_DIM);
 }
 
-int getCH(void) {
+int getCH(void)
+{
     ch = getch();
     if (ch == KEY_ESCAPE){
         endwin();
@@ -32,9 +50,24 @@ int getCH(void) {
     return ch;
 }
 
-void cross(void) {
+void cross(void)
+{
     //croix multi centrée pour giga centrage actif
     mvvline(0, COLS/2, ACS_VLINE, LINES);
     mvhline(LINES/2, 0, ACS_HLINE, COLS);
     mvaddch(LINES/2, COLS/2, ACS_PLUS);
+}
+
+void erase_cursor(cursorPos_t cursorPos)
+{
+    move(cursorPos.bigY + cursorPos.smallY, cursorPos.bigX + cursorPos.smallX);
+    addch(' ');
+}
+
+void print_cursor(cursorPos_t cursorPos)
+{
+    move(cursorPos.bigY + cursorPos.smallY, cursorPos.bigX + cursorPos.smallX);
+    attron(A_STANDOUT | COLOR_PAIR(1));
+        addch(' ');
+    attroff(A_STANDOUT | COLOR_PAIR(1));
 }
